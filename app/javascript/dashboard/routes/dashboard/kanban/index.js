515054a4007462ff8850ea778e003638db749562
Victor <vitgermim@gmail.com>
2025-08-17 12:55:17 -0300

    fix: import

--- app/javascript/dashboard/routes/dashboard/kanban/index.js
+++ app/javascript/dashboard/routes/dashboard/kanban/index.js
@@ -3,6 +3,6 @@ export default [
     path: 'kanban',
     name: 'kanban_index',
     meta: { requiresAuth: true },
-    component: () => import('dashboard/routes/kanban/KanbanBoard.vue'),
+    component: () => import('dashboard/routes/dashboard/kanban/KanbanBoard.vue'),
   },
 ];
